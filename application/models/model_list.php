0ba4d41c7a2a3e1b65caa2561169cf6fe4a4146d
sersergei <sersergei@bigmir.net>
2014-07-08 17:32:43 +0300

    PDO

--- application/models/model_list.php
+++ application/models/model_list.php
@@ -1,55 +1,66 @@
 <?php
     class Model_list extends Model{
         function list_edd($name,$user){
-            $result=Model::connect()->query("INSERT INTO list(name,user_id) VALUES (:name,:user)");
+            $result=Model::connect()->prepare("INSERT INTO list(name,user_id) VALUES (:name,:user)");
             $result->execute($data);
             return $result;
         }
         function list_view($id){
-<<<<<<< HEAD
             $result['result']=Model::connect()->query("SELECT * FROM `list` WHERE id='$id'");
-=======
             $result=Model::connect()->query("SELECT * FROM `list` WHERE id='$id'");
-            $result=mysql_fetch_array($result);
->>>>>>> 44daa994e1b88b75a9ad8c62570c8f3569f69ae7
+            $result=$result->fetch();
             return $result;
         }
         function list_goods($id){
-            $result=Model::connect()->query("SELECT * FROM `goods` WHERE id_list='$id'");
-            return $result;           
+            $result=Model::connect()->prepare("SELECT * FROM `goods` WHERE id_list=?");
+            $result->execute($id);
+            return $result;
         }
         function list_add_goods($arr,$id_list){
-            $result=Model::query("INSERT INTO goods(name_goods,price,measure,qti,id_list ) VALUES('{$arr['name_goods']}','{$arr['price']}','{$arr['measure']}','{$arr['qti']}','$id_list')");
+            $arr['id_list']=$id_list;
+            $result=Model::connect()->prepare("INSERT INTO goods(name_goods,price,measure,qti,id_list) VALUES(:name_goods,:price,:measure,:qti,:id_list)");
+            $result->execute($arr);
             return $result;
         }
         function list_del_goods($id){
-            $result=Model::query("DELETE FROM `goods` WHERE `id`='$id'");
+            $result=Model::connect()->prepare("DELETE FROM `goods` WHERE `id`=:id");
+            $result->execute($id);
         }
         function list_del($id){
-            $result=Model::query("DELETE FROM `list` WHERE `id`='$id'");
-            if($result){
-                $result=Model::query("DELETE FROM `goods` WHERE id_list='$id' ");
+            $result=Model::connect()->query("DELETE FROM `list` WHERE `id`=:id");
+            if($result->execute($id)){
+                $result=Model::connect()->query("DELETE FROM `goods` WHERE id_list=:id ");
+                $result->execute($id);
             }
             return $result;
         }
         function user($email){
-            $result=Model::connect()->query("SELECT `id` FROM `users` WHERE email='$email'");
+            $result=Model::connect()->prepare("SELECT `id` FROM `users` WHERE email=:email");
+            $result->execute($email);
             return $result;
         }
         function user_list_valid($user_id,$list_id){
-            $result=Model::connect()->query("SELECT * FROM list_user WHERE user_id='$user_id' and list_id='$list_id'");
+            $result=Model::connect()->prepare("SELECT * FROM list_user WHERE user_id=? and list_id=?");
+            $result->execute(array($user_id,$list_id));
             return $result;
             
         }
         function list_user_add($user_id,$list_id){
-            $result=Model::connect()-> query("INSERT INTO list_user(user_id,list_id) VALUES('$user_id','$list_id')");
+            $result=Model::connect()->prepare("INSERT INTO list_user(user_id,list_id) VALUES(?,?)");
+            $result->execute(array($user_id,$list_id));
             return $result;
         }
         function list_bloc($id,$date,$bloc_user_id,$bloc=1){
-            $result=Model::connect()->query("UPDATE `list`SET `bloc`='$bloc', `last_upd`='$date', `bloc_user_id`='$bloc_user_id' WHERE `id`='$id' ");
+            $result=Model::connect()->prepare("UPDATE `list`SET `bloc`=:bloc, `last_upd`=:date, `bloc_user_id`=:bloc_user_id WHERE `id`=:id ");
+            $result->bindParam(":bloc",$bloc);
+            $result->bindParam(":date",$date);
+            $result->bindParam(":bloc_user_id",$bloc_user_id);
+            $result->bindParam(":id",$id);
+            $result->execute();
         }
           function list_ajax($id,$date){
-            $result=Model::query("UPDATE `list`SET `bloc`='1', `last_upd`='$date'  WHERE `id`='$id' ");
+            $result=Model::connect()->prepare("UPDATE `list`SET `bloc`='1', `last_upd`=?  WHERE `id`=? ");
+            $result->execute(array($date,$id));
         }
          
     }
--- application/models/model_user.php
+++ application/models/model_user.php
@@ -5,16 +5,19 @@
             $result->execute($data);
         }
         public function avto($arr){
-            $result=Model::connect()->query("SELECT `id`,`login` FROM `users` WHERE email='{$arr['email']}' and password='{$arr['password']}'");
+            $result=Model::connect()->prepare("SELECT `id`,`login` FROM `users` WHERE email=:email and password=password");
+            $result->execute($arr);
             $result=$result->fetch();
             return $result;
         }
         function select_list($user){
-            $result=Model::connect()->query("SELECT `id`,`name` FROM `list` WHERE user_id='$user'");
+            $result=Model::connect()->prepare("SELECT `id`,`name` FROM `list` WHERE user_id=?");
+            $result->execute($user);
             return $result;
         }
         function select_list_part($id) {
-           $result=Model::connect()->query(" SELECT *FROM list l INNER JOIN list_user u ON u.list_id = l.id WHERE u.user_id = '$id'");
+           $result=Model::connect()->prepare(" SELECT *FROM list l INNER JOIN list_user u ON u.list_id = l.id WHERE u.user_id =?");
+           $result->execute($id);
            return $result;
         }
     }
